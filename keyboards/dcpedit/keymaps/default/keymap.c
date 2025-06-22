92bd98cd257152a3f49607d4c5cc016dfc2062f4
Lucas Friesen <135739244+LJFRIESE@users.noreply.github.com>
2025-06-22 14:34:48 -0700

    Update keymap.c

--- keyboards/dcpedit/keymaps/default/keymap.c
+++ keyboards/dcpedit/keymaps/default/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_LSFT    ,KC_Z       ,KC_X       ,KC_C       ,KC_D       ,KC_V                                                      ,KC_K       ,KC_H       ,KC_COMM    ,KC_DOT     ,KC_SLSH    ,KC_RSFT,
                   KC_HOME   ,KC_PGUP     ,KC_PGDN    ,KC_END                                                                           ,KC_LEFT    ,KC_DOWN    ,KC_UP      ,KC_RGHT,
                                                                   KC_LCTL   ,KC_LALT                            ,KC_LALT      ,KC_RCTL
-                                                                            ,KC_HYPR                            ,KC_HYPR
+                                                                            ,KC_LGUI                            ,KC_RGUI
                                                      ,KC_BSPC     ,KC_DEL   ,KC_MEH                             ,KC_MEH      ,KC_ENTER   ,KC_SPC
     ),
     [4] = LAYOUT(
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       _______    ,XXXXXXX    ,KC_F10     ,KC_F11     ,KC_F12     ,XXXXXXX                                                    ,XXXXXXX    ,KC_NUM     ,KC_PEQL    ,KC_PSLS    ,KC_PAST    ,XXXXXXX,
       _______    ,XXXXXXX    ,KC_F7      ,KC_F8      ,KC_F9      ,XXXXXXX                                                    ,XXXXXXX    ,KC_P7      ,KC_P8      ,KC_P9      ,KC_PMNS    ,XXXXXXX,
       _______    ,XXXXXXX    ,KC_F4      ,KC_F5      ,KC_F6      ,XXXXXXX                                                    ,XXXXXXX    ,KC_P4      ,KC_P5      ,KC_P6      ,KC_PPLS    ,XXXXXXX,
-      _______    ,XXXXXXX    ,KC_F1      ,KC_F2      ,KC_F3      ,XXXXXXX                                                    ,XXXXXXX    ,KC_P1      ,KC_P2      ,KC_P3      ,KC_PENT    ,XXXXXXX,
+      _______    ,XXXXXXX    ,KC_F1      ,KC_F2      ,KC_F3      ,XXXXXXX                                                    ,KC_P0      ,KC_P1      ,KC_P2      ,KC_P3      ,KC_PENT    ,XXXXXXX,
                   XXXXXXX    ,XXXXXXX    ,XXXXXXX    ,XXXXXXX                                                                            ,_______    ,_______    ,_______    ,_______
                                                                  ,_______   ,_______                            ,_______    ,_______
                                                                             ,_______                            ,_______
